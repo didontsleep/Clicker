dbbcb8beebfaa42cfb42e46e978741eac1df0a5d
yan <kusochek.humora@gmail.com>
2021-05-05 07:39:34 +0600

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,16 @@
 $('.advert__case').on("click", function () {
     $('.advert__case-item').toggleClass('_active-case')
 })
+//первый кейс 
 $('.case__content-item').on("click", function () {
     $(this).find('.case__content-img').toggleClass('_active-case')
 })
+// вторые кейсы 
 $('.burger').on("click", function () {
     $('.burger-block').fadeIn('burger__active')
 })
+// бургер открытватся
 $('.close-btn').on("click", function () {
     $('.burger-block').fadeOut('burger__active')
 })
+// закрывается
